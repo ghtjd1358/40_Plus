9ebfd508c6c5b9afdc49389a04cc1769bc195950
dydgjs200 <dydgjs2016@naver.com>
2023-12-22 13:56:42 +0900

    serverless

--- controllers/Cboard.js
+++ controllers/Cboard.js
@@ -8,7 +8,7 @@ exports.board = (req, res) => {
 };
 
 // 게시물 등록 -> title, content 값 등록
-exports.postDB = (req, res) => {
+exports.postDB = (req, res, next) => {
   const { title, content } = req.body;
   console.log("axios get data > ", req.body);
 
@@ -16,13 +16,17 @@ exports.postDB = (req, res) => {
     userid: req.session.userid,
     title: title,
     content: content,
-  }).then((result) => {
-    console.log("postDB 완료 > ", result);
-  });
+  })
+    .then((result) => {
+      console.log("postDB 완료 > ", result);
+    })
+    .catch((error) => {
+      return next(error);
+    });
 };
 
 // 게시물 전체 출력
-exports.boardList = (req, res) => {
+exports.boardList = (req, res, next) => {
   CommunityTable.findAll({ raw: true })
     .then((result) => {
       if (result) {
@@ -33,11 +37,12 @@ exports.boardList = (req, res) => {
     .catch((error) => {
       console.error("Error:", error);
       res.render("./board/boardList", { success: false, error: error.message });
+      return next(error);
     });
 };
 
 //댓글 추가
-exports.writeComment = (req, res) => {
+exports.writeComment = (req, res, next) => {
   const { foreign_number, content } = req.body;
 
   console.log("writeComment 값 받기 > ", req.body);
@@ -46,39 +51,51 @@ exports.writeComment = (req, res) => {
     userid: req.session.userid,
     content: content,
     foreign_number: foreign_number,
-  }).then((result) => {
-    console.log("댓글 추가 > ", result);
-  });
+  })
+    .then((result) => {
+      console.log("댓글 추가 > ", result);
+    })
+    .catch((error) => {
+      return next(error);
+    });
 };
 
 // 댓글 읽어오기 -> foreign number(게시물 번호) 이용
-exports.readComment = (req, res) => {
+exports.readComment = (req, res, next) => {
   console.log("req.body > ", req.body);
   const number = req.body.number;
 
   console.log("readComment number > ", number);
   CommentTable.findAll({
     where: { foreign_number: number },
-  }).then((result) => {
-    const commentArray = [];
-    result.forEach((comment) => {
-      // commentArray = [userid, content] 배열
-      commentArray.push([
-        comment.dataValues.userid,
-        comment.dataValues.content,
-      ]);
-    });
+  })
+    .then((result) => {
+      const commentArray = [];
+      result.forEach((comment) => {
+        // commentArray = [userid, content] 배열
+        commentArray.push([
+          comment.dataValues.userid,
+          comment.dataValues.content,
+        ]);
+      });
 
-    console.log("Comment Array >  ", commentArray);
-    res.send({ commentArray: commentArray });
-  });
+      console.log("Comment Array >  ", commentArray);
+      res.send({ commentArray: commentArray });
+    })
+    .catch((error) => {
+      return next(error);
+    });
 };
 
 // 임시 댓글 출력용
-exports.readAllComment = (req, res) => {
+exports.readAllComment = (req, res, next) => {
   CommentTable.findAll({
     raw: true,
-  }).then((result) => {
-    console.log("All comment > ", result);
-  });
+  })
+    .then((result) => {
+      console.log("All comment > ", result);
+    })
+    .catch((error) => {
+      return next(error);
+    });
 };
--- serverless_socket/app.js
+++ serverless_socket/app.js
@@ -3,70 +3,100 @@ const AWS = require("aws-sdk");
 
 const lambda = new AWS.Lambda({
   region: "ap-northeast-2", // Lambda 함수가 속한 리전으로 변경
-  accessKeyId: "AKIA2SP45FJULY6JEDXI",
-  secretAccessKey: "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",
+  accessKeyId: "AKIA2SP45FJUHMQHHHPU",
+  secretAccessKey: "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",
 });
 
 const WebSocketURL =
-  "wss://q2ggee9bte.execute-api.ap-northeast-2.amazonaws.com/dev/"; // AWS API Gateway WebSocket API의 엔드포인트로 변경
+  "wss://plkcqxmi24.execute-api.ap-northeast-2.amazonaws.com/dev/"; // AWS API Gateway WebSocket API의 엔드포인트로 변경
+
+const connectHandler =
+  "websocket-api-chat-app-tuto-ConnectHandler2FFD52D8-6cMbnVsnSYRc";
+const sendmessageHandler =
+  "websocket-api-chat-app-tu-SendMessageHandlerDCEABF-1jeF26CMsVwg";
+const disconnectHandler =
+  "websocket-api-chat-app-tu-DisconnectHandlerCB7ED6F-LshTNgmzkCUu";
 
 // WebSocket 연결 생성
-const ws = new WebSocket(WebSocketURL);
+let ws;
 
-ws.on("open", () => {
-  console.log("WebSocket connection opened");
+// 홈페이지 구성
+const express = require("express");
+const app = express();
+const PORT = 8000;
 
-  // Connect 이벤트 시뮬레이션
-  lambda.invoke(
-    {
-      FunctionName:
-        "websocket-api-chat-app-tuto-ConnectHandler2FFD52D8-KzMHDG4oMYNn", // Connect 핸들러를 실행하는 Lambda 함수 이름으로 변경
-      InvocationType: "Event",
-      Payload: JSON.stringify({
-        requestContext: {
-          connectionId: "your-connection-id", // 임의의 connectionId 값을 사용하거나, 실제로 저장된 connectionId 값을 사용
+app.set("view engine", "ejs");
+app.set("views", "./views");
+
+app.get("/", (req, res) => {
+  console.log("메인홈페이지 입장");
+
+  // WebSocket 연결 생성 (처음 한 번만)
+  if (!ws) {
+    ws = new WebSocket(WebSocketURL);
+
+    ws.on("open", () => {
+      console.log("WebSocket connection opened");
+
+      // Connect 이벤트 시뮬레이션
+      lambda.invoke(
+        {
+          FunctionName: connectHandler,
+          InvocationType: "RequestResponse", // 동기 호출
         },
-      }),
-    },
-    (err, data) => {
-      if (err) {
-        console.error("Error calling Connect Lambda function:", err);
-      } else {
-        console.log("Connect Lambda function called successfully:", data);
-
-        // SendMessage 이벤트 시뮬레이션
-        const messageData = {
-          action: "sendmessage",
-          message: "Hello from the JavaScript code!",
-        };
-
-        lambda.invoke(
-          {
-            FunctionName:
-              "websocket-api-chat-app-tu-SendMessageHandlerDCEABF-DI3lX25xweCl", // SendMessage 핸들러를 실행하는 Lambda 함수 이름으로 변경
-            InvocationType: "Event",
-            Payload: JSON.stringify(messageData),
-          },
-          (err, data) => {
-            if (err) {
-              console.error("Error calling SendMessage Lambda function:", err);
-            } else {
-              console.log(
-                "SendMessage Lambda function called successfully:",
-                data
-              );
-            }
+        (err, data) => {
+          if (err) {
+            console.error("Error calling Connect Lambda function:", err);
+          } else {
+            console.log("Lambda response:", data);
+            console.log("lambda payload > ", data.Payload);
           }
-        );
+        }
+      );
+    });
+
+    ws.on("message", (data) => {
+      console.log("Received message from server:", data);
+    });
+
+    ws.on("close", () => {
+      console.log("WebSocket connection closed");
+      ws = null; // 연결이 종료되면 ws 객체 초기화
+    });
+  }
+
+  res.render("index");
+});
+
+// 채팅방 떠나기
+app.get("/leave", (req, res) => {
+  if (ws) {
+    console.log("disconnect handler on");
+    lambda.invoke(
+      {
+        FunctionName: disconnectHandler,
+        InvocationType: "Event",
+        Payload: JSON.stringify({
+          requestContext: {
+            connectionId: "your-connection-id", // 현재 연결된 WebSocket의 connectionId 값을 사용
+          },
+        }),
+      },
+      (err, data) => {
+        if (err) {
+          console.error("Error calling Disconnect Lambda function:", err);
+        } else {
+          console.log("Disconnect Lambda function called successfully:", data);
+
+          // WebSocket 연결 종료
+          ws.close();
+        }
       }
-    }
-  );
+    );
+  }
+  res.render("leave");
 });
 
-ws.on("message", (data) => {
-  console.log("Received message from server:", data);
-});
-
-ws.on("close", () => {
-  console.log("WebSocket connection closed");
+app.listen(PORT, function () {
+  console.log("server open");
 });
