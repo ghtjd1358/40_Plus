84b2834bbe16c653d7ec544883a972178b168b71
dydgjs200 <dydgjs2016@naver.com>
2023-12-26 09:01:55 +0900

    connect community

--- controllers/community.controller.js
+++ controllers/community.controller.js
@@ -40,62 +40,64 @@ const { Op } = require("sequelize");
 
 //댓글 추가
 exports.writeComment = (req, res, next) => {
-  const { foreign_number, content } = req.body;
+  const { number, content } = req.body;
 
   console.log("writeComment 값 받기 > ", req.body);
-  console.log(foreign_number, content);
-  CommentTable.create({
-    userid: req.session.userid,
-    content: content,
-    foreign_number: foreign_number,
-  })
-    .then((result) => {
+  console.log(number, content);
+  console.log("number type > ", typeof number);
+  console.log("session > ", req.session.userid);
+
+  if (content != "") {
+    //무플 방지..
+    CommentTable.create({
+      userid: req.session.userid,
+      content: content,
+      foreign_number: parseInt(number),
+    }).then((result) => {
       console.log("댓글 추가 > ", result);
-    })
-    .catch((error) => {
-      return next(error);
     });
+  }
 };
 
 // 댓글 읽어오기 -> foreign number(게시물 번호) 이용
-exports.readComment = (req, res, next) => {
-  console.log("req.body > ", req.body);
-  const number = req.body.number;
+// exports.readComment = (req, res, next) => {
+//   console.log("req.body > ", req.body);
+//   const number = req.body.number;
 
-  console.log("readComment number > ", number);
-  CommentTable.findAll({
-    where: { foreign_number: number },
-  })
-    .then((result) => {
-      const commentArray = [];
-      result.forEach((comment) => {
-        // commentArray = [userid, content] 배열
-        commentArray.push([
-          comment.dataValues.userid,
-          comment.dataValues.content,
-        ]);
-      });
+//   console.log("readComment number > ", number);
+//   CommentTable.findAll({
+//     where: { foreign_number: number },
+//   })
+//     .then((result) => {
+//       const commentArray = [];
+//       result.forEach((comment) => {
+//         // commentArray = [userid, content] 배열
+//         commentArray.push([
+//           comment.dataValues.userid,
+//           comment.dataValues.content,
+//         ]);
+//       });
 
-      console.log("Comment Array >  ", commentArray);
-      res.send({ commentArray: commentArray });
-    })
-    .catch((error) => {
-      return next(error);
-    });
-};
+//       console.log("Comment Array >  ", commentArray);
+//       res.send({ commentArray: commentArray });
+//     })
+//     .catch((error) => {
+//       return next(error);
+//     });
+// };
 
 // 임시 댓글 출력용
-exports.readAllComment = (req, res, next) => {
-  CommentTable.findAll({
-    raw: true,
-  })
-    .then((result) => {
-      console.log("All comment > ", result);
-    })
-    .catch((error) => {
-      return next(error);
-    });
-};
+// exports.readAllComment = (req, res, next) => {
+//   CommentTable.findAll({
+//     raw: true,
+//   })
+//     .then((result) => {
+//       console.log("All comment > ", result);
+//     })
+//     .catch((error) => {
+//       return next(error);
+//     });
+// };
 
 //
 // 아래는 게시판 기능
@@ -118,6 +120,7 @@ exports.readCommunity = (req, res) => {
   const number = req.query.number;
 
   CommunityTable.findOne({ where: { number: number } }).then((result) => {
+    console.log("read comm > ", result.dataValues);
     res.render("community/read", { data: result.dataValues });
   });
 };
@@ -178,5 +181,6 @@ exports.communityPost = (req, res) => {
     content: content,
   }).then((result) => {
     console.log("post create");
+    res.send({ createSuccess: true });
   });
 };
