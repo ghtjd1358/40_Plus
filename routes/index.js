f35db4478c31f5ad81760fde014a23fe4a4413c8
dydgjs200 <dydgjs2016@naver.com>
2023-12-26 13:20:27 +0900

    댓글달기 수정

--- routes/index.js
+++ routes/index.js
@@ -45,6 +45,9 @@ router.get("/writeCommunity", controller.writeCommunity);
 router.get("/readCommunity", controller.readCommunity);
 router.post("/detailCommunityPage", controller.detailCommunityPage);
 
+// 글 삭제
+router.delete("/deleteCommunity", controller.deleteCommunity);
+
 // 댓글 관련
 router.post("/writeComment", controller.writeComment);
 
